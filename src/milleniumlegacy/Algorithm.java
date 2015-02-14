693b11fc2a9aa995530de3bf31f3180835404130
Ryan Wise <ryan.wise@wiseweb-design.com>
2015-02-14 15:00:57 -0500

    hi

---
+++ src/milleniumlegacy/Algorithm.java
@@ -0,0 +1,13 @@
+package milleniumlegacy;
+
+public class Algorithm {
+String item1 = "hi";
+String item2 = "bye";
+	public static String research(String item1, String item2) {
+		System.out.println(item1);
+		return item2;
+	}
+	
+
+
+}
